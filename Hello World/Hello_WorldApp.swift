6b9a7c0b84f83beeeefcca760c9df473ac0fed07
BoiseITGuru <dev@boiseitguru.com>
2022-08-27 13:13:17 -0600

    created xcode project

---
+++ Hello World/Hello_WorldApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hello_WorldApp.swift
+//  Hello World
+//
+//  Created by BoiseITGuru on 8/27/22.
+//
+
+import SwiftUI
+
+@main
+struct Hello_WorldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
